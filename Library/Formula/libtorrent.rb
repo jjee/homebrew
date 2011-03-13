f64b0bccc345c9efa24d863453f677b3cccc40b4
Jason Jee <jj41190@gmail.com>
2011-03-12 23:48:51 -0800

    added kqueue patch for libtorrent in FreeBSD systems

--- Library/Formula/libtorrent.rb
+++ Library/Formula/libtorrent.rb
@@ -9,7 +9,8 @@ class Libtorrent < Formula
   depends_on 'libsigc++'
 
   def install
-    system "patch -p0 ./src/data/memory_chunk.cc /usr/local/Cellar/mem.patch"
+    system "patch -p0 ./src/data/memory_chunk.cc /usr/local/Cellar/libtorrent/mem.patch"
+    system "patch -p0 ./src/torrent/poll_kqueue.cc /usr/local/Cellar/libtorrent/poll_kqueue_freebsd.patch"
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-kqueue", "--enable-ipv6"
     system "make"
     system "make install"
