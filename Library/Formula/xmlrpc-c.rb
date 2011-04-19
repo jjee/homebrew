0c8509bc5fbaf326c934c092c85bae7f1b793550
Jason Jee <jj41190@gmail.com>
2011-04-19 13:57:19 -0700

    moved xmlrpc-c to advanced branch

--- Library/Formula/xmlrpc-c.rb
+++ Library/Formula/xmlrpc-c.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class XmlrpcC <Formula
-  url 'http://xmlrpc-c.svn.sourceforge.net/svnroot/xmlrpc-c/stable'
+  url 'http://xmlrpc-c.svn.sourceforge.net/svnroot/xmlrpc-c/advanced'
   homepage 'http://xmlrpc-c.sourceforge.net/'
   version 'foo'
 
@@ -11,6 +11,7 @@ class XmlrpcC <Formula
     # link in expat
     #NOTE seemingly it isn't possible to build dylibs with this thing
     system "./configure", "--disable-debug", "--enable-libxml2-backend", "--prefix=#{prefix}"
+    system "make"
     system "make install"
   end
 end
